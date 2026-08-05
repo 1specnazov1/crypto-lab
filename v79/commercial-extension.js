af6bb8c9524fd8b52e7d5f61f1e2345537d50bc0
1specnazov1 <1specnazov1@gmail.com>
2026-08-05 09:47:23 +0300

    Wire protected auth gateway into account page

--- v79/commercial-extension.js
+++ v79/commercial-extension.js
@@ -9,6 +9,7 @@
       const doc=frame.contentDocument,path=frame.contentWindow.location.pathname;if(!doc)return;
       if(path.endsWith('/account.html')){
         add(doc,'registrationConsentScript','./registration-consent.js');
+        add(doc,'protectedAuthGatewayScript','./auth-gateway.js');
         add(doc,'commercialCenterScript','./commercial.js');
         add(doc,'subscriptionLifecycleScript','./subscription-lifecycle.js');
         add(doc,'sessionSecurityScript','./session-security.js');
